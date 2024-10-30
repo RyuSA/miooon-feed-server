8838d82e1094e96b427f1852fdf6fd5aa5dfcdac
RyuSA <12961775+RyuSA@users.noreply.github.com>
2024-10-30 16:41:46 +0000

    Refactor FirehoseSubscription to use a filter chain for post creation

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,3 +1,4 @@
+import { FilterChain, MioFilter } from './algos/filter';
 import {
   OutputSchema as RepoEvent,
   isCommit,
@@ -5,39 +6,45 @@ import {
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
+
+  private filterChain: FilterChain;
+
+  constructor(public db, public service, filterChain?: FilterChain) {
+    super(db, service);
+    this.filterChain = filterChain || MioFilter;
+  }
+
   async handleEvent(evt: RepoEvent) {
-    if (!isCommit(evt)) return
-    const ops = await getOpsByType(evt)
+    if (!isCommit(evt)) return;
+    const ops = await getOpsByType(evt);
 
-    console.log('handling commit')
+    const postsToDelete = ops.posts.deletes.map((del) => del.uri);
+    const prePostsToCreate = ops.posts.creates.filter((create) => this.filterChain.apply(create.record));
 
-    const postsToDelete = ops.posts.deletes.map((del) => del.uri)
-    const postsToCreate = ops.posts.creates
-      .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
-      })
-      .map((create) => {
-        // map alf-related posts to a db row
-        return {
-          uri: create.uri,
-          cid: create.cid,
-          indexedAt: new Date().toISOString(),
-        }
-      })
+    const postsToCreate = prePostsToCreate
+      .sort((a, b) => new Date(a.record.createdAt).getTime() - new Date(b.record.createdAt).getTime())
+      .map((create) => ({
+        uri: create.uri,
+        cid: create.cid,
+        createdAt: create.record.createdAt,
+        indexedAt: new Date().toISOString(),
+      }));
 
     if (postsToDelete.length > 0) {
       await this.db
         .deleteFrom('post')
         .where('uri', 'in', postsToDelete)
-        .execute()
+        .execute();
     }
+
     if (postsToCreate.length > 0) {
       await this.db
         .insertInto('post')
         .values(postsToCreate)
-        .onConflict((oc) => oc.doNothing())
-        .execute()
+        .onConflict((oc) => {
+          return oc.doNothing();
+        })
+        .execute();
     }
   }
 }
